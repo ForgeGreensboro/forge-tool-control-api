3ecc870f7fb47f51d503137a0b3a0e6a15581b3a
Chris Warner <chris@quadland.com>
2016-11-05 15:29:33 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # forge-tool-control-api
 Forge Access Control
+
+To start: docker-compose up in the root of this repo.  It will bring up postgres and the webserver.  Once it is up, you can go to http://localhost:8000 to bring up the root.
+
---
+++ forge_tool_access/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for forge_tool_access project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forge_tool_access.settings")
+
+application = get_wsgi_application()
